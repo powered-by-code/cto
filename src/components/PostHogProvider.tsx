c68fc6ca2347266a81a99eb27ce485b38550f1a1
Ruben Shekspir <shekspir55@gmail.com>
2025-04-21 19:15:48 +0400

    env bacl

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -5,9 +5,11 @@ import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react"
 import { Suspense, useEffect } from "react"
 import { usePathname, useSearchParams } from "next/navigation"
 
+import { env } from "@/env"
+
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    posthog.init( env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: "https://us.i.posthog.com",
       person_profiles: 'always',
       capture_pageview: true, // We capture pageviews manually
